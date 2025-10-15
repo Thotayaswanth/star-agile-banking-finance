661cb24b236d803787f936c61b0fc9b0aa02d549
Thotayaswanth <thotayaswanth115@gmail.com>
2025-10-15 14:45:46 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,30 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
-  key_name = "kav"
+  ami                    = "ami-0360c520857e3138f"
+  instance_type          = "t2.micro"
+  key_name               = "kav"
   vpc_security_group_ids = ["sg-02a0b65f22e3b57aa"]
+
   connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./kav.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./kav.pem")
+    host        = self.public_ip
+  }
+
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = ["echo 'wait to start the instance'"]
   }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/project-finance/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server"
   }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[testservers]" > inventory.ini
+      echo "${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=./kav.pem" >> inventory.ini
+      ansible-playbook -i inventory.ini /var/lib/jenkins/workspace/project-finance/terraform-files/ansibleplaybook.yml
+      rm inventory.ini
+    EOT
+  }
+}
